fa758a67cb26f382e7a1aae27d95d8b8ec88dde8
Mattijs Ugen <mattijsugen@gmail.com>
2012-02-10 18:10:25 +0100

    Remove temp main()

--- Source/nl/utwente/cs/pxml/PXML.java
+++ Source/nl/utwente/cs/pxml/PXML.java
@@ -84,10 +84,4 @@ public abstract class PXML {
 		return probability;
 	}
 
-	public static void main(String... args) {
-		for (String arg : args) {
-			System.out.println(consistent(arg));
-		}
-	}
-
 }
