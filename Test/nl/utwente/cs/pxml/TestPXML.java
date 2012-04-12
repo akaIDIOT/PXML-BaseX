f7a7f4b2a713213ed7ef50d7d37bdbca03e87d37
Mattijs Ugen <mattijsugen@gmail.com>
2012-04-12 09:55:59 +0200

    Add JUnit.jar and TextPXML class

---
+++ Test/nl/utwente/cs/pxml/TestPXML.java
@@ -0,0 +1,36 @@
+package nl.utwente.cs.pxml;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPXML {
+	
+	protected PXML subject;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.subject = new PXML();
+	}
+
+	@Test
+	public void testCombine() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsistent() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMutuallyExclusive() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProbability() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
