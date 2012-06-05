a61e8d195fd2a533cfe5a505b77ecda13daed034
Mattijs Ugen <mattijsugen@gmail.com>
2012-06-05 15:35:06 +0200

    Prelimiary 'working' version
    Threw StAX overboard; information about the document is needed during
    transformation, so DOM is the only (and slow) choice. Tried to copy as many
    features from XMLToPXMLTransformer as possible, some TODO's remain.

--- Source/nl/utwente/cs/pxml/transform/DocumentTransformer.java
+++ Source/nl/utwente/cs/pxml/transform/DocumentTransformer.java
@@ -5,22 +5,29 @@ import static nl.utwente.cs.pxml.ProbabilityNodeType.INDEPENDENT;
 import static nl.utwente.cs.pxml.ProbabilityNodeType.MUTEX;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.xml.stream.XMLEventFactory;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLEventWriter;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.Namespace;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathException;
+import javax.xml.xpath.XPathFactory;
 
 import nl.utwente.cs.pxml.ProbabilityNodeType;
-import nl.utwente.cs.pxml.util.CompoundIterator;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class DocumentTransformer {
 
@@ -34,13 +41,15 @@ public class DocumentTransformer {
 	protected ProbabilityNodeType[] pNodeDistribution = {
 			MUTEX, MUTEX, MUTEX, MUTEX, INDEPENDENT, INDEPENDENT, INDEPENDENT, INDEPENDENT, EVENTS
 	};
+	// the number of random variables relative to the expected number of EVENT-type pNodes
+	protected float pVariablesRatio = 0.1f;
 
 	// TODO: incorporate _ratioExpSubsets, _maxExpSubsetsPwr and _ratioCieRVars from XMLToPXMLTransformer.java
 
-	protected XMLEventFactory events;
+	protected XPath xpath;
 
 	public DocumentTransformer() {
-		this.events = XMLEventFactory.newInstance();
+		this.xpath = XPathFactory.newInstance().newXPath();
 	}
 
 	public DocumentTransformer(float pNodesOccurrence, ProbabilityNodeType[] pNodeDistribution) {
@@ -49,82 +58,114 @@ public class DocumentTransformer {
 		this.pNodeDistribution = pNodeDistribution;
 	}
 
-	public void transform(XMLEventReader in, XMLEventWriter out) throws DocumentTransformerException {
+	public void transform(Document doc) throws DocumentTransformerException {
 		try {
-			// pass events up to the root element (the first StartElement)
-			XMLEvent current = in.nextEvent();
-			while (!current.isStartElement()) {
-				out.add(current);
-				current = in.nextEvent();
+			// add the pxml namespace to the document
+			doc.getDocumentElement().setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:" + NS_PREFIX, NS_URI);
+			
+			// find all the nodes in the document element (make sure to not wrap the root)
+			NodeList nodes = (NodeList) xpath.evaluate("//*", doc.getDocumentElement(), XPathConstants.NODESET);
+			int length = nodes.getLength();
+			int numVariables = this.determineNumVariables(length);
+			System.out.println("  document has " + length + " cadidate nodes");
+
+			// select length * occurrence nodes at random
+			for (int i = 0, max = (int) (length * this.pNodesOccurrence); i < max; i++) {
+				Node el = nodes.item((int) (Math.random() * length));
+				// select a random number of preceding/following siblings to wrap together
+				List<Node> selected = this.selectNodes(el);
+				// create a pNode to wrap the selected elements with (make sure to create exactly enough attributes)
+				Node pNode = this.createPNode(doc, selected.size(), numVariables);
+				// replace the first selected node with the newly created pNode
+				Node firstSelected = selected.get(0);
+				firstSelected.getParentNode().replaceChild(pNode, firstSelected);
+				// attach all the selected nodes to the pNode now in the document
+				// (appending a node will move it if it is already in the DOM (TODO: check this))
+				for (Node node : selected) {
+					pNode.appendChild(node);
+				}
 			}
 
-			this.events.createNamespace(NS_PREFIX, NS_URI);
-
-			// current is now a StartElement representing the root element, add the namespace to it
-			StartElement root = this.addNamespaces(current.asStartElement(), events.createNamespace(NS_PREFIX, NS_URI));
-			out.add(root);
-
-			// children of root are next, proceed with probability elements insertion
-			this.insertElements(in, out, 1); // only root element was added, current depth is 1
-
-			// finish the document after element insertion
-			while (in.hasNext()) {
-				out.add(in.nextEvent());
-			}
-			out.close();
-		} catch (XMLStreamException e) {
-			throw new DocumentTransformerException("error parsing xml stream: " + e.getMessage(), e);
+			// add the used random variables to the document
+			// TODO
+		} catch (XPathException e) {
+			throw new DocumentTransformerException("Unexpected XPath error while transforming: " + e.getMessage(), e);
 		}
 	}
 
-	protected void insertElements(XMLEventReader in, XMLEventWriter out, int currentDepth) throws XMLStreamException {
-		int startingDepth = currentDepth;
-
-		// read the next event (always added)
-		XMLEvent current = in.nextEvent();
-		switch (current.getEventType()) {
-			case XMLStreamConstants.START_ELEMENT: {
-				out.add(current);
-				// element opening increases depth by 1
-				currentDepth++;
-				if (in.peek().getEventType() != XMLStreamConstants.END_ELEMENT) {
-					// encountered an element that has children (no immediate end element), chance to insert something
-					// TODO
-				}
-				break;
-			}
-			case XMLStreamConstants.END_ELEMENT: {
-				out.add(current);
-				// element closing decreases depth by 1
-				currentDepth--;
-				if (currentDepth == startingDepth && in.peek().getEventType() == XMLStreamConstants.END_ELEMENT) {
-					// starting depth reached and next event closes this level: this call is done
-					return;
-				}
-				break;
-			}
-			default: {
-				// default to 'copying' the input
-				out.add(current);
+	protected int determineNumVariables(int numNodes) {
+		// count the number of times EVENTS occurs in the distribution
+		int occurrence = 0;
+		for (ProbabilityNodeType type : this.pNodeDistribution) {
+			if (type == EVENTS) {
+				occurrence++;
 			}
 		}
+
+		// calculate the number of variables from the number of expected EVENTS-type pNodes
+		return (int) (numNodes * pNodesOccurrence * ((float) occurrence / this.pNodeDistribution.length) * this.pVariablesRatio);
 	}
 
-	@SuppressWarnings("unchecked")
-	public StartElement addNamespaces(StartElement element, Namespace... namespaces) {
-		return this.events.createStartElement(element.getName(), element.getAttributes(), new CompoundIterator<Object>(
-				element.getNamespaces(), (Object[]) namespaces));
+	protected List<Node> selectNodes(Node el) {
+		// default to child index 0 (...)
+		int index = 0;
+		// (...) but try to find the actual child index
+		NodeList children = el.getParentNode().getChildNodes();
+		for (int i = 0, max = children.getLength(); index == 0 && i < max; i++) {
+			if (children.item(i) == el) {
+				// el found, update index
+				index = i;
+			}
+		}
+
+		// determine the number of siblings to select (× 2 to get the *average* at pNodesSiblings)
+		int numSiblings = (int) (Math.random() * children.getLength());
+		// make sure to not select too many elements but at least one
+		numSiblings = Math.max(Math.min(numSiblings, children.getLength()), 1);
+
+		// determine the starting index (but it should be at least zero)
+		int startIndex = Math.max(index - (numSiblings / 2), 0);
+		int endIndex = Math.min(startIndex + numSiblings, children.getLength() - 1);
+
+		// TODO: fix end - start not being equal to num?
+
+		// gather the actual nodes
+		List<Node> selected = new ArrayList<Node>(numSiblings);
+		for (int i = startIndex; i <= endIndex; i++) {
+			selected.add(children.item(i));
+		}
+		// return the selected nodes
+		return selected;
 	}
 
+	protected Node createPNode(Document origin, int numChilds, int numVariables) {
+		// select a type at random
+		ProbabilityNodeType type = this.pNodeDistribution[(int) (Math.random() * this.pNodeDistribution.length)];
+		// create a new node of the required type
+		Node node = origin.createElementNS(NS_URI, NS_PREFIX + ":" + type.nodeName);
+		
+		// return the newly created node
+		return node;
+	}
+	
 	public static void main(String... args) {
 		try {
-			XMLEventReader in = XMLInputFactory.newInstance().createXMLEventReader(
-					new FileInputStream(new File("/tmp/input.xml")));
-			XMLEventWriter out = XMLOutputFactory.newInstance().createXMLEventWriter(
-					new FileOutputStream(new File("/tmp/output.xml")));
-
-			new DocumentTransformer().transform(in, out);
-		} catch (Exception e) { // IEUW!
+			Document input = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File("/tmp/input.xml"));
+			new DocumentTransformer().transform(input);
+			TransformerFactory.newInstance().newTransformer().transform(new DOMSource(input), new StreamResult(new File("/tmp/output.xml")));
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (DocumentTransformerException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
 			e.printStackTrace();
 		}
 	}
